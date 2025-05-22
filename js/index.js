44363779da836e1f6694ed652df1a2d2cded9c24
11bellawu11 <qwu025@uottawa.ca>
2025-05-21 23:58:30 -0400

    fixed footer

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 function goHome() {
     document.querySelector("#home").style.display= "block";
     document.querySelector("#more-me").style.display= "none";
-    document.querySelector("#game").style.display= "none";
     document.querySelector("#workflow").style.display= "none";
     document.querySelector("#projects").style.display= "none";
-    document.querySelector("#footer2").style.display= "block";
+    document.querySelector(".footer2").style.display= "block";
 
 }
 
@@ -12,26 +11,26 @@ function goHome() {
 function HomeExpanded() {
     document.querySelector("#home").style.display= "block";
     document.querySelector("#more-me").style.display= "none";
-    document.querySelector("#game").style.display= "none";
     document.querySelector("#workflow").style.display= "none";
     document.querySelector("#projects").style.display= "grid";
-    document.querySelector("#footer").style.display= "block"
-    document.querySelector("#footer2").style.display= "none"
+    document.querySelector(".footer2").style.display= "none"
 
 }
     
 function goMe() {
     document.querySelector("#home").style.display= "none";
     document.querySelector("#more-me").style.display= "block";
-    document.querySelector("#game").style.display= "none";
     document.querySelector("#workflow").style.display= "none";
+    document.querySelector("#projects").style.display= "none";
+    document.querySelector("#footer2").style.display= "none";
 
 }
 
 function goWorkflow() {
     document.querySelector("#home").style.display= "none";
     document.querySelector("#more-me").style.display= "none";
-    document.querySelector("#game").style.display= "none";
     document.querySelector("#workflow").style.display= "block";
+    ocument.querySelector("#projects").style.display= "none";
+    document.querySelector("#footer2").style.display= "none";
 
 }
